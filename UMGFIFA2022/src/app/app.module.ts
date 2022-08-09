f90e33e4b26324ba1270dae541322da61d3524da
Erwins14 <erwins1414@gmail.com>
2022-08-08 23:33:05 -0600

    Creación del Componente Sedes

--- UMGFIFA2022/src/app/app.module.ts
+++ UMGFIFA2022/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { SedesComponent } from './sedes/sedes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EncabezadoComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    SedesComponent
   ],
   imports: [
     BrowserModule,
