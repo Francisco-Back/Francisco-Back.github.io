5cc6646e37012748068dc794ff82b033f77b8c34
annabellaaroche <110144837+annabellaaroche@users.noreply.github.com>
2022-08-08 20:04:22 -0600

    Set Nav bar, Home page, general design
    Set Nav var with boostrap, home page, and general design with images.
    
    Using boostrap

--- UMGFIFA2022/src/app/app.module.ts
+++ UMGFIFA2022/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
