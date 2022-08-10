7f6d7ed00a4a2582d83b54ec09ed006f0b3530c0
annabellaaroche <110144837+annabellaaroche@users.noreply.github.com>
2022-08-09 21:06:59 -0600

    Pagina Paises
    Cambio de fondo y implementacion Ngfor

--- UMGFIFA2022/src/app/paises/paises.component.ts
+++ UMGFIFA2022/src/app/paises/paises.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paises.component.css']
 })
 export class PaisesComponent implements OnInit {
-
+  paises: Array<any>=[
+    {nombre:'Japon',bandera:'https://s1.significados.com/foto/bandera-japon_sm.png',jugadores:'26',numfifa:'25'},
+    {nombre:'Mexico',bandera:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/1280px-Flag_of_Mexico.svg.png',jugadores:'26',numfifa:'25'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
