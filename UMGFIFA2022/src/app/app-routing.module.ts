1f019878e38bd8d3622036bd274401427af1134c
Erwins14 <erwins1414@gmail.com>
2022-08-17 22:09:56 -0600

    Encabezado (Sedes)

--- UMGFIFA2022/src/app/app-routing.module.ts
+++ UMGFIFA2022/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SedesComponent } from './sedes/sedes.component';
 const routes: Routes = [
-  
+
   { path: '', component: HomeComponent ,pathMatch:'full'},
+  { path: 'sedes', component: SedesComponent ,pathMatch:'full'},
   { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
 
