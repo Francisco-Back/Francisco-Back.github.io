acd2c314a1ebe455165499dc2d545eb875084728
MarcodPG98 <mg32834@gmail.com>
2022-08-18 22:44:31 -0600

    Firebase_GruposVF

--- UMGFIFA2022/src/app/group/group.component.ts
+++ UMGFIFA2022/src/app/group/group.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import {Database,set,ref,update,onValue} from '@angular/fire/database';
 
 @Component({
@@ -6,10 +7,17 @@ import {Database,set,ref,update,onValue} from '@angular/fire/database';
   templateUrl: './group.component.html',
   styleUrls: ['./group.component.css']
 })
+
 export class GroupComponent implements OnInit {
 
-  equipos: Array<any>=[]
-  equipos1: Array<any>=[]
+  grupoA: Array<any>=[]
+  grupoB: Array<any>=[]
+  grupoC: Array<any>=[]
+  grupoD: Array<any>=[]
+  grupoE: Array<any>=[]
+  grupoF: Array<any>=[]
+  grupoG: Array<any>=[]
+  grupoH: Array<any>=[]
 
   constructor(public database:Database) { }
 
@@ -23,7 +31,7 @@ export class GroupComponent implements OnInit {
       snapshot.forEach((childSnapshot) => {
         const childData = childSnapshot.val();
         let equipo ={nombre:childData.nombre,bandera:childData.img,grupo:childData.grupo};
-        this.equipos.push(equipo);
+        this.grupoA.push(equipo);
       });
     });
 
@@ -32,7 +40,61 @@ export class GroupComponent implements OnInit {
       snapshot.forEach((childSnapshot) => {
         const childData = childSnapshot.val();
         let equipo1 ={nombre:childData.nombre,bandera:childData.img,grupo:childData.grupo};
-        this.equipos1.push(equipo1);
+        this.grupoB.push(equipo1);
+      });
+    });
+
+    const starCountRef2 = ref(this.database, 'Equipos/GrupoC/');
+    onValue(starCountRef2, (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        const childData = childSnapshot.val();
+        let equipo1 ={nombre:childData.nombre,bandera:childData.img,grupo:childData.grupo};
+        this.grupoC.push(equipo1);
+      });
+    });
+
+    const starCountRef3 = ref(this.database, 'Equipos/GrupoD/');
+    onValue(starCountRef3, (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        const childData = childSnapshot.val();
+        let equipo1 ={nombre:childData.nombre,bandera:childData.img,grupo:childData.grupo};
+        this.grupoD.push(equipo1);
+      });
+    });
+
+    const starCountRef4 = ref(this.database, 'Equipos/GrupoE/');
+    onValue(starCountRef4, (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        const childData = childSnapshot.val();
+        let equipo1 ={nombre:childData.nombre,bandera:childData.img,grupo:childData.grupo};
+        this.grupoE.push(equipo1);
+      });
+    });
+
+    const starCountRef5 = ref(this.database, 'Equipos/GrupoF/');
+    onValue(starCountRef5, (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        const childData = childSnapshot.val();
+        let equipo1 ={nombre:childData.nombre,bandera:childData.img,grupo:childData.grupo};
+        this.grupoF.push(equipo1);
+      });
+    });
+
+    const starCountRef6 = ref(this.database, 'Equipos/GrupoG/');
+    onValue(starCountRef6, (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        const childData = childSnapshot.val();
+        let equipo1 ={nombre:childData.nombre,bandera:childData.img,grupo:childData.grupo};
+        this.grupoG.push(equipo1);
+      });
+    });
+
+    const starCountRef7 = ref(this.database, 'Equipos/GrupoH/');
+    onValue(starCountRef7, (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        const childData = childSnapshot.val();
+        let equipo1 ={nombre:childData.nombre,bandera:childData.img,grupo:childData.grupo};
+        this.grupoH.push(equipo1);
       });
     });
   }
