7c7e15925d09f04300276e84fca6cb392d9909d4
annabellaaroche <110144837+annabellaaroche@users.noreply.github.com>
2022-08-18 18:09:31 -0600

    Pagina Sedes
    Pagina sedes, agregue ruta a App.Routing y app.module, asi como encabezado y home component.

--- UMGFIFA2022/src/app/app-routing.module.ts
+++ UMGFIFA2022/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { GroupComponent } from './group/group.component';
 import { HomeComponent } from './home/home.component';
 import { PaisesComponent } from './paises/paises.component';
+import { SedesComponent } from './sedes/sedes.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent ,pathMatch:'full'},
   { path: 'paises', component: PaisesComponent },
   { path: 'grupos', component: GroupComponent },
+  { path: 'sedes', component: SedesComponent },
   { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
 
