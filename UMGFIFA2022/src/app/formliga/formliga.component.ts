92ba7978bd6efd181bb4faee3f3b3f6fe1b53f12
MarcodPG98 <mg32834@gmail.com>
2022-09-15 17:54:03 -0600

    validaciones

--- UMGFIFA2022/src/app/formliga/formliga.component.ts
+++ UMGFIFA2022/src/app/formliga/formliga.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formliga',
@@ -8,8 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class FormligaComponent implements OnInit {
 
   constructor() { }
+  liga = new FormGroup({
+    nombreLiga: new FormControl('',Validators.required),
+    fechaInicio: new FormControl('', Validators.required),
+    fechaFin: new FormControl('', Validators.required),
+    ciudad: new FormControl('', Validators.required),
+    cantEquipos: new FormControl('',Validators.required)
+  });
 
   ngOnInit(): void {
+  }
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.liga.value);
   }
 
 }
