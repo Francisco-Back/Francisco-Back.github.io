b98b7e6d87b7a9e0a3e299137369f0410ca265c5
Erwins14 <erwins1414@gmail.com>
2022-09-15 19:34:53 -0600

    Pagina Confirmación Liga

--- src/app/confirmacionliga/confirmacionliga.component.ts
+++ src/app/confirmacionliga/confirmacionliga.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import {Database,set,ref,update,onValue} from '@angular/fire/database';
+import { push } from '@firebase/database';
 
 @Component({
   selector: 'app-confirmacionliga',
   templateUrl: './confirmacionliga.component.html',
   styleUrls: ['./confirmacionliga.component.css']
 })
-export class ConfirmacionligaComponent implements OnInit {
 
-  constructor() { }
+export class ConfirmacionligaComponent implements OnInit {
+ligas: Array<any>=[]
+usuarios: Array<any>=[]
+  constructor(public database:Database) { }
 
   ngOnInit(): void {
-  }
+      this.obtenerLiga();
+      this.obtenerUsuario();
+    }
+
+  registrarLiga(){}
+
+  obtenerLiga(){
+    const starCountRef = ref(this.database, 'Ligas/');
+    onValue(starCountRef, (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        const childData = childSnapshot.val();
+        let liga ={id:childData.id,nombre:childData.nombre,cantidadEquipos:childData.cantidadEquipos,fechaInicio:childData.fechaInicio,fechaFinal:childData.fechaFinal};
+        this.ligas.push(liga);
+      });
+  });
+    }
+
+    obtenerUsuario(){
+        const starCountRef = ref(this.database, 'Usuarios/');
+        onValue(starCountRef, (snapshot) => {
+          snapshot.forEach((childSnapshot) => {
+            const childData = childSnapshot.val();
+            let usuario ={id:childData.id,nombre:childData.nombre,apellido:childData.apellido,pais:childData.pais};
+            this.usuarios.push(usuario);
+          });
+      });
+        }
 
 }
+
