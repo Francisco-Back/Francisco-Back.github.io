417268d4c585e19ed92579c172a46f34ce57bdc0
Na So <panfilo-gordito@hotmail.com>
2022-09-20 21:46:56 -0600

    Pantalla principal completa

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Database, onValue, ref } from '@angular/fire/database';
+import { Router } from '@angular/router';
+import { Liga } from '../models/liga.model';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-principal',
@@ -6,10 +10,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent implements OnInit {
+  Jornada1: Array<any>=[]
+  Jornada2: Array<any>=[]
+  Jornada3: Array<any>=[]
+  ligas: Liga[]=[];
+  userID=13;
+  constructor(
+    public database:Database,
+    public userService: UserService,
+    private router: Router) { }
 
-  constructor() { }
+  
 
   ngOnInit(): void {
+    this.obtenerPartidos();
+    this.obtenerLiga();
   }
 
+
+  obtenerPartidos(){
+
+  const starJor1 = ref(this.database, 'Partidos/Jornada1/');
+  onValue(starJor1, (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const Data1 = childSnapshot.val();
+      let partidos ={bandera1:Data1.Bandera1,bandera2:Data1.Bandera2,pais1:Data1.Pais1,
+        pais2:Data1.Pais2, horario:Data1.Horario, fecha:Data1.Fecha,grupos1:Data1.Tamaño};
+      this.Jornada1.push(partidos);
+    });
+
+
+   
+});
+const starJor2 = ref(this.database, 'Partidos/Jornada2/');
+  onValue(starJor2, (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const Data1 = childSnapshot.val();
+      let partidos ={bandera1:Data1.Bandera1,bandera2:Data1.Bandera2,pais1:Data1.Pais1,
+        pais2:Data1.Pais2, horario:Data1.Horario, fecha:Data1.Fecha,grupos1:Data1.Tamaño};
+      this.Jornada2.push(partidos);
+    });
+
+   
+});
+ 
+const starJor3 = ref(this.database, 'Partidos/Jornada3/');
+  onValue(starJor3, (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const Data1 = childSnapshot.val();
+      let partidos ={bandera1:Data1.Bandera1,bandera2:Data1.Bandera2,pais1:Data1.Pais1,
+        pais2:Data1.Pais2, horario:Data1.Horario, fecha:Data1.Fecha,grupos1:Data1.Tamaño};
+      this.Jornada3.push(partidos);
+    });
+
+  
+});
+
+  }
+  obtenerLiga(){
+    this.userService.ligasFindByUserId(this.userID).subscribe((data: Liga[])=>{
+        this.ligas = data;
+        console.log(this.ligas);
+    });
+    }  
 }
