aa785c074fd440f8ab76b7110a1264601730ebfa
Francisco-Back <fbacks@miumg.edu.gt>
2022-10-15 23:20:24 -0600

    Revert "Servicio Partidos"
    This reverts commit b9b1f4d9f86c82730d827343f5bf97835cbdb21a.

--- src/app/infrastructure/env.ts
+++
@@ -1,5 +0,0 @@
-export const Env = {
-  messageBrokerAddress: process.env['RABBITMQ_ADDRESS'] || 'amqps://b-4a2daddb-b41f-4300-8a32-cd29dcb45bca.mq.us-east-2.amazonaws.com:5671'
-  /*messageBrokerAddress: process.env['RABBITMQ_ADDRESS'] || 'amqp://guest:guest@localhost:5672'*/
-
-}
