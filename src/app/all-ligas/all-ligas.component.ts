34824e705e9088d813a0d8497d59b6b0e815899a
annabellaaroche <110144837+annabellaaroche@users.noreply.github.com>
2022-10-20 20:54:17 -0600

    changes - alll ligas

--- src/app/all-ligas/all-ligas.component.ts
+++ src/app/all-ligas/all-ligas.component.ts
@@ -17,7 +17,7 @@ export class AllLigasComponent implements OnInit {
   searchText: any;
   userID!: number ;
   ligas: Array<any>=[];
-  ligaUser:Array<any>=[];
+  ligaUser:Array<Liga>=[];
   constructor( 
     public userService: UserService,
     private userIDService: UserIDService,
@@ -35,14 +35,11 @@ export class AllLigasComponent implements OnInit {
      data.forEach((childSnapshot) => {
       const Data1 = childSnapshot;       
       if(Data1.usuario.id!=this.userID){
-        this.ligaUser.forEach((element)=>{
-          if(element!=Data1.id){
           let liga = Data1;
           this.ligas.push(liga);
         }
-        })
-      }
     });
+    
     }, error =>{
       this.toastr.error('Error de Servidor:'+error.status, 'Fail', {
       timeOut: 6000,  positionClass: 'toast-top-center',
@@ -68,8 +65,9 @@ export class AllLigasComponent implements OnInit {
     this.userService.UserLigasFindByUser(this.userID).subscribe((data:LigaUser[])=>{
       data.forEach((childSnapshot) => {
         const Data1 = childSnapshot;       
-        let LigasUser=Data1.ligasEntity.id;
-        this.ligaUser.push(LigasUser);        
+        let LigasUser=Data1.ligasEntity;
+        this.ligaUser.push(LigasUser);
+        console.log(this.ligaUser);
       });
     });
   }
